c1103185229d180cefc0b20d9dc116ec4e47c73b
Aryan <aryankumar91237@gmail.com>
2025-11-05 09:19:39 +0530

    project initiated

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,10 @@
+package ticket.booking.entities;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private String dateOfTravel;
+    private Train train;
+}
